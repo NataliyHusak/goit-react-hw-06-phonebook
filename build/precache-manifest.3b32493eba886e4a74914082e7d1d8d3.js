bdd1d8dcb09bc3defd075b1042d529438925c372
NataliyHusak <50527604+NataliyHusak@users.noreply.github.com>
2020-03-24 22:28:29 +0200

    01

---
+++ build/precache-manifest.3b32493eba886e4a74914082e7d1d8d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d997f86160608721204f3a883e641b16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c745373674d02acc4c1",
+    "url": "/static/css/2.4087c797.chunk.css"
+  },
+  {
+    "revision": "0ab2a6c0058c0da5e901",
+    "url": "/static/css/main.579ef23b.chunk.css"
+  },
+  {
+    "revision": "2c745373674d02acc4c1",
+    "url": "/static/js/2.08432036.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.08432036.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ab2a6c0058c0da5e901",
+    "url": "/static/js/main.16b94dc0.chunk.js"
+  },
+  {
+    "revision": "bfddb6ef0e1b13526b53",
+    "url": "/static/js/runtime-main.c4041058.js"
+  }
+]);
